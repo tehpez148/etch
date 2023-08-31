38830e111859a1acfffca3e2017aaa13d9622160
fatman148 <108585064+tehpez148@users.noreply.github.com>
2023-08-31 19:04:46 +0100

    playing around with create the grid with a button event lister. Can now create and destroy, but only with set value. Need to work out how to change value. May use slider?

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.getElementById('grid');
 
-
 var s = parseInt(document.getElementById("sides").value);
 
-
+const newGridButton = document.querySelector('#new');
+newGridButton.addEventListener('click', function() {
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+    
+    makeDivs((s*s));
+    console.log(s);
+});
 var x = document.querySelector('#grid');
 x.style.setProperty('--x',(s));
 
@@ -16,12 +22,6 @@ for (let d=0; d < numDivs; d++){
     const cells = document.createElement('div');
     cells.classList.add('cells');
     container.appendChild(cells);
-
-}
-}
-
-
-makeDivs((s*s))
-
+}};
 
 
